3645f21b5e65749732d093738dd9fd39d5e54c07
Gitarackur <peterosah5@gmail.com>
2026-03-25 11:07:37 +0100

    update

--- mods/nemotron-super/run.sh
+++ mods/nemotron-super/run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 set -e
-cd $WORKSPACE_DIR
-wget https://huggingface.co/nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4/resolve/main/super_v3_reasoning_parser.py
+# cd $WORKSPACE_DIR
+# wget https://huggingface.co/nvidia/NVIDIA-Nemotron-3-Super-120B-A12B-NVFP4/resolve/main/super_v3_reasoning_parser.py
+
+
+
+# MY MOD OF RHE MODS (from resolution error on wget downloading it directly from huggingface)
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+FILE="$SCRIPT_DIR/super_v3_reasoning_parser.py"
+
+if [[ ! -f "$FILE" ]]; then
+   echo "File not found: $FILE"
+   exit 1
+fi
+
+cp "$FILE" "$WORKSPACE_DIR/"
+
+
